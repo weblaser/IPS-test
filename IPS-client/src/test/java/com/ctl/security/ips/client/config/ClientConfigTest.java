b885ed92b259525c378fd0c96799f9da7173749a
kweber-security <kevin.weber@centurylink.com>
2014-12-31 16:55:27 -0600

    adding test coverage

--- IPS-client/src/test/java/com/ctl/security/ips/client/config/ClientConfigTest.java
+++ IPS-client/src/test/java/com/ctl/security/ips/client/config/ClientConfigTest.java
@@ -1,8 +1,32 @@
 package com.ctl.security.ips.client.config;
 
+import org.junit.Test;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.Assert.assertNotNull;
+
 /**
  * Created by Kevin.Weber on 10/29/2014.
  */
 public class ClientConfigTest {
 
+    ClientConfig underTest = new ClientConfig();
+
+    @Test
+    public void testPropertySourcesPlaceholderConfigurer() {
+        //act
+        PropertySourcesPlaceholderConfigurer actual = ClientConfig.propertySourcesPlaceholderConfigurer();
+
+        //assert
+        assertNotNull(actual);
+    }
+
+    @Test
+    public void testRestTemplate() {
+        //act
+        RestTemplate actual = underTest.restTemplate();
+        //assert
+        assertNotNull(actual);
+    }
 }
