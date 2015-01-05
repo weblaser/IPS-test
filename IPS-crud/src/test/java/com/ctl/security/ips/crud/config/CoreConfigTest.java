689cb8f93213e7864bc2fbf9aa7cbe450a8a88fe
kweber-security <kevin.weber@centurylink.com>
2015-01-05 14:09:25 -0600

    SECURITY-176 adding test coverage

--- IPS-crud/src/test/java/com/ctl/security/ips/crud/config/CoreConfigTest.java
+++ IPS-crud/src/test/java/com/ctl/security/ips/crud/config/CoreConfigTest.java
@@ -1,6 +1,13 @@
 package com.ctl.security.ips.crud.config;
 
 
+import org.junit.Test;
+
 public class CoreConfigTest {
+
+    @Test
+    public void testConstructor() {
+        new CoreConfig();
+    }
 
 }
