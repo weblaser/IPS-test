ed390cdda75163bf238cec7a108a822cccc675eb
kweber-security <kevin.weber@centurylink.com>
2015-01-05 11:43:32 -0600

    using lombak in common

--- IPS-common/src/main/java/com/ctl/security/ips/common/domain/Policy.java
+++ IPS-common/src/main/java/com/ctl/security/ips/common/domain/Policy.java
@@ -1,5 +1,10 @@
 package com.ctl.security.ips.common.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -7,61 +12,16 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 /**
  * Created by Kevin.Weber on 10/27/2014.
  */
+@ToString
+@EqualsAndHashCode()
+@Accessors(chain = true)
 public class Policy {
 
+    @Getter
+    @Setter
     private String id;
+
+    @Getter
+    @Setter
     private PolicyStatus status;
-
-    public String getId() {
-        return id;
-    }
-
-    public Policy setId(String id) {
-        this.id = id;
-        return this;
-    }
-
-    public PolicyStatus getStatus() {
-        return status;
-    }
-
-    public Policy setStatus(PolicyStatus status) {
-        this.status = status;
-        return this;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null) {
-            return false;
-        }
-        if (o == this) {
-            return true;
-        }
-        if (o.getClass() != getClass()) {
-            return false;
-        }
-        Policy rhs = (Policy) o;
-        return new EqualsBuilder()
-                .append(id, rhs.id)
-                .append(status, rhs.status)
-                .isEquals();
-    }
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder()
-                .append(id)
-                .append(status)
-                .toHashCode();
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this)
-                .append("id", id)
-                .append("status", status)
-                .toString();
-    }
-
 }
