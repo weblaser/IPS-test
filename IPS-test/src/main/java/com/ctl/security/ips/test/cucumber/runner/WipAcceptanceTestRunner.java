af46b44285c2b8ae93f400ddec93d18e04b7abe9
kevinstl <kjwilde@hotmail.com>
2015-01-16 16:33:44 -0600

    Running all cukes.

---
+++ IPS-test/src/main/java/com/ctl/security/ips/test/cucumber/runner/WipAcceptanceTestRunner.java
@@ -0,0 +1,40 @@
+package com.ctl.security.ips.test.cucumber.runner;
+
+import cucumber.api.junit.Cucumber;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+//@Cucumber.Options(tags = { "@ALL_TESTS" }, monochrome = true, format = {"html:target/cucumber", "junit:target/junit.xml", "json:target/cucumber-report.json"},
+@Cucumber.Options(tags = {"@WIP"}, monochrome = true, format = {"html:target/cucumber", "junit:target/junit.xml", "json:target/cucumber-report.json"},
+        features = {"src/main/java/com/ctl/security/ips/test/cucumber/feature"},
+        glue ={"com/ctl/security/ips/test/cucumber/step"}
+)
+// NOTES:
+// 	1) To only run UI tests add "tags = { "@UI" }, " into Cucumber.Options
+//  2) To only run API tests add "tags = { "@API" }, " into Cucumber.Options
+//  3) To run UI tests requiring setup, use:  tags = { "@WIP_UI, @SETUP"}
+//	4) To run all tests remove the "tags" argument
+// 
+// example of a "not" tag => tags = { "~@WIP" }
+// can use -Dcucumber.options="--format {\"pretty\", \"html:target/cucumber\", \"junit:target/junit.xml\"} --glue classpath:com/savvis src/test/resources"
+public class WipAcceptanceTestRunner {
+    public WipAcceptanceTestRunner() {
+    }
+
+    @BeforeClass
+    public static void setup() {
+        // Do NOT start selenium here, only start it based on @Before(value="@UI"), so only a feature file that has @UI causes it to start
+        // The @Before is in the UnifyComputeSystemSteps class)
+//		WebDriverController.getDriver();
+
+    }
+
+    @AfterClass
+    public static void teardown() {
+        // stop the Selenium web driver
+//		WebDriverController.stopDriver();
+    }
+}
---
+++ IPS-test/src/main/java/com/ctl/security/ips/test/cucumber/step/CreatePolicySteps.java
@@ -0,0 +1,25 @@
+package com.ctl.security.ips.test.cucumber.step;
+
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration("classpath*:cucumber.xml")
+public class CreatePolicySteps {
+
+    @Given("^I have a policy that I want to create in DSM$")
+    public void i_have_a_policy_that_I_want_to_create_in_DSM() throws Throwable {
+    }
+
+    @When("^I execute the \"(.*?)\" operation against the DSM API$")
+    public void i_execute_the_operation_against_the_DSM_API(String arg1) throws Throwable {
+    }
+
+    @Then("^I receive a response from DSM that contains the expected policy$")
+    public void i_receive_a_response_from_DSM_that_contains_the_expected_policy() throws Throwable {
+    }
+}
+
