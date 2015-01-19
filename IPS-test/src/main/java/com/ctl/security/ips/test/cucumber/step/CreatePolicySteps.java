86b603dccfeafbbea401e5d35b55c2739ed7f334
kevinstl <kjwilde@hotmail.com>
2015-01-19 16:17:06 -0600

    Adding wiremock.

--- IPS-test/src/main/java/com/ctl/security/ips/test/cucumber/step/CreatePolicySteps.java
+++ IPS-test/src/main/java/com/ctl/security/ips/test/cucumber/step/CreatePolicySteps.java
@@ -1,6 +1,8 @@
 package com.ctl.security.ips.test.cucumber.step;
 
 import com.ctl.security.dsm.authenticate.DsmPolicyClient;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import cucumber.api.java.en.Given;
 
 
@@ -10,6 +12,11 @@ import manager.SecurityProfileTransport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -39,7 +46,24 @@ public class CreatePolicySteps {
 
     @When("^I execute the \"(.*?)\" operation against the DSM API$")
     public void i_execute_the_operation_against_the_DSM_API(String arg1) throws Throwable {
+
+        WireMockServer wireMockServer = new WireMockServer(wireMockConfig().port(8089)); //No-args constructor will start on port 8080, no HTTPS
+        wireMockServer.start();
+
+        WireMock.configureFor("localhost", 8089);
+
+        try{
+        stubFor(get(urlEqualTo("/some/thing"))
+//            stubFor(get(urlEqualTo("/"))
+                    .willReturn(aResponse()
+                            .withHeader("Content-Type", "text/plain")
+                            .withBody("Hello world!")));
+
         newlyCreatedSecurityProfileTransport = policyClient.createPolicyOnDSMClient(username, password, securityProfileTransportToBeCreated);
+        }
+        finally{
+            wireMockServer.stop();
+        }
     }
 
     @Then("^I receive a new policy response$")
