3fb5e4f2e16569819fa86ca48f0f23079eb6b500
kevinstl <kjwilde@hotmail.com>
2015-02-26 13:45:11 -0600

    Providing reference to security.data.client.properties.

--- ips-service/src/main/java/com/ctl/security/ips/service/config/CoreConfig.java
+++ ips-service/src/main/java/com/ctl/security/ips/service/config/CoreConfig.java
@@ -2,10 +2,12 @@ package com.ctl.security.ips.service.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 
 @Configuration
 @ComponentScan(basePackages = "com.ctl.security.ips.crud")
+@PropertySource("classpath:properties/security.data.client.properties")
 public class CoreConfig {
 
 }
