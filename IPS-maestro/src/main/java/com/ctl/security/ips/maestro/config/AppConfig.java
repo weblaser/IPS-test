31c17b4ffe8934f57ed0b19afdba88fa5dfdbf27
kevinstl <kjwilde@hotmail.com>
2015-02-26 13:00:43 -0600

    Implemented amq for delete.

--- IPS-maestro/src/main/java/com/ctl/security/ips/maestro/config/AppConfig.java
+++ IPS-maestro/src/main/java/com/ctl/security/ips/maestro/config/AppConfig.java
@@ -2,9 +2,11 @@ package com.ctl.security.ips.maestro.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = {"com.ctl.security"})
+@PropertySource("classpath:properties/security.data.client.properties")
 public class AppConfig {
 
 }
