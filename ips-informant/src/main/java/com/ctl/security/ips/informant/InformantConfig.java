947beb919461b2661d000c9f4a49715f85fd3ae1
SeanRobb <sean.robb@centurylink.com>
2015-03-27 14:44:48 -0500

    Save Point

--- ips-informant/src/main/java/com/ctl/security/ips/informant/InformantConfig.java
+++ ips-informant/src/main/java/com/ctl/security/ips/informant/InformantConfig.java
@@ -2,12 +2,14 @@ package com.ctl.security.ips.informant;
 
 import com.ctl.security.clc.client.core.config.ClcClientCoreAppConfig;
 import com.ctl.security.ips.dsm.config.DsmConfig;
+import com.ctl.security.library.common.config.SecurityLibraryCommonAppConfig;
 import org.quartz.impl.StdSchedulerFactory;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 /**
  * Created by Sean Robb on 3/24/2015.
