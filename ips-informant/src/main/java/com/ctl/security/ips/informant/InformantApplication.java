6844f8bbb0835c7752fb6c33537989c998df8b9d
gary.hebel <gary.hebel@centurylink.com>
2015-04-08 15:38:39 -0500

    added init.d script, added repackage goal in maven

--- ips-informant/src/main/java/com/ctl/security/ips/informant/InformantApplication.java
+++ ips-informant/src/main/java/com/ctl/security/ips/informant/InformantApplication.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 /**
@@ -13,15 +13,16 @@ import org.springframework.context.annotation.Import;
  */
 
 @EnableAutoConfiguration
+@Configuration
 @Import(InformantConfig.class)
 public class InformantApplication {
     private static Logger logger = LogManager.getLogger(InformantApplication.class);
 
     public static void main(String[] args) {
 
-        ApplicationContext applicationContext = SpringApplication.run(InformantApplication.class, args);
+        logger.info("Starting Informant...");
+        SpringApplication.run(InformantApplication.class, args);
 
-        logger.info("Informant Started...");
     }
 
 }
