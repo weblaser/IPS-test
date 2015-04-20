c711f0a98c6859655b94334c8d150a9e8b28b8c9
kevinstl <kjwilde@hotmail.com>
2015-04-20 15:55:34 -0500

    Improving logging in NotificationResourceImpl.

--- IPS-resource/src/main/java/com/ctl/security/ips/api/resource/impl/NotificationResourceImpl.java
+++ IPS-resource/src/main/java/com/ctl/security/ips/api/resource/impl/NotificationResourceImpl.java
@@ -4,6 +4,8 @@ import com.ctl.security.data.common.domain.mongo.NotificationDestination;
 import com.ctl.security.ips.api.jms.NotificationMessageSender;
 import com.ctl.security.ips.api.resource.NotificationResource;
 import com.ctl.security.ips.common.jms.bean.NotificationDestinationBean;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +19,22 @@ import java.util.List;
 @Component
 public class NotificationResourceImpl implements NotificationResource {
 
+    private Logger logger = LogManager.getLogger(NotificationResourceImpl.class);
+
     @Autowired
     private NotificationMessageSender notificationMessageSender;
 
     @Override
     public void updateNotificationDestination(String account, String hostName, List<NotificationDestination> notificationDestinations) {
+
+        logger.error("updateNotificationDestination begin");
+        logger.error("account: " + account );
+        logger.error("hostName: " + hostName);
+        logger.error("notificationDestinations: " + notificationDestinations);
+
         NotificationDestinationBean notificationDestinationBean = new NotificationDestinationBean(hostName, account, notificationDestinations);
         notificationMessageSender.updateNotificationDestination(notificationDestinationBean);
+
+        logger.error("updateNotificationDestination end");
     }
 }
