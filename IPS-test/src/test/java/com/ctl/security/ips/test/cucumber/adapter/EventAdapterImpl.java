3a47f01079ae5313710a40ab9616a31a59875547
SeanRobb <sean.robb@centurylink.com>
2015-05-14 12:40:15 -0500

    Added logging to Informant and to Event Adapter

--- IPS-test/src/test/java/com/ctl/security/ips/test/cucumber/adapter/EventAdapterImpl.java
+++ IPS-test/src/test/java/com/ctl/security/ips/test/cucumber/adapter/EventAdapterImpl.java
@@ -2,6 +2,8 @@ package com.ctl.security.ips.test.cucumber.adapter;
 
 import com.ctl.security.data.common.domain.mongo.ConfigurationItem;
 import com.ctl.security.ips.common.domain.Event.DpiEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,23 +12,24 @@ import java.util.List;
 
 /**
  * Created by Sean Robb on 3/30/2015.
- *
  */
 
 public class EventAdapterImpl implements EventAdapter {
+
+    private static final Logger logger = LogManager.getLogger(EventAdapterImpl.class);
 
     @Override
     public void triggerDpiEvent(ConfigurationItem configurationItem, List<DpiEvent> dpiEvents) {
 
         try {
-            String dip = configurationItem.getHostName(); // ip address
-            int port = 4321;
-            PrintWriter output = new PrintWriter(new Socket(dip, port).getOutputStream());
+            logger.info("Attacking " + configurationItem.getHostName());
+            PrintWriter output = new PrintWriter(new Socket(configurationItem.getHostName(), 4321).getOutputStream());
             output.print("magicstring");
             output.flush();
             output.close();
+            logger.info("Attack Complete...");
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error(e);
         }
     }
 }
